8caad705c03032a68d411f581b7e72abe99b3d40
Juan Soto Shee <129634747+juan-soto-shee@users.noreply.github.com>
2026-07-12 22:15:52 -0400

    Ajustar objetivos KPI y auditoría de importación

--- plantview-admin/admin.js
+++ plantview-admin/admin.js
@@ -8,7 +8,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { doc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 import { initializeDeleteHistory } from "./modules/deleteHistory.js";
-import { initializeBulkImport } from "./modules/bulkImport.js";
+import { initializeBulkImport } from "./modules/bulkImport.js?v=20260713-2";
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
