dc4b433feef1469b3f078e036ecf1d585d1fb346
Juan Soto Shee <129634747+juan-soto-shee@users.noreply.github.com>
2026-07-17 20:39:11 +0000

    Conectar PlantView con API predictiva en Cloud Run

--- js/modelingConfig.js
+++ js/modelingConfig.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL = globalThis.PLANTVIEW_PREDICTIVE_API_URL || "http://127.0.0.1:8000";
+export const BASE_API_URL = globalThis.PLANTVIEW_PREDICTIVE_API_URL || "https://plantview-predictive-api-708512240285.southamerica-west1.run.app";
 export const MODELING_API_TIMEOUT_MS = 15000;
